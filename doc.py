b0ab1437607ccc9ececa69f2744094a217a36c1e
Hayden Heroux <haydenheroux@gmail.com>
2023-05-18 23:34:53 -0400

    WIP Documentation tool working commit.

--- doc.py
+++ doc.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from typing import List, NamedTuple
+from typing import List, NamedTuple, Optional
 
 def get_lines(filename: str) -> List[str]:
     lines = list()
@@ -9,48 +9,79 @@ def get_lines(filename: str) -> List[str]:
     return lines
 
 class Doc(NamedTuple):
-    name: str
-    return_type: str
-    parameters: List[str]
-    documentation: str
+    description: Optional[List[str]]
+    name: Optional[str]
+    parameters: Optional[List[str]]
+    return_type: Optional[str]
 
 
 def fmt_doc(doc: Doc) -> str:
-    return f"{doc.return_type} {doc.name} {doc.documentation} {'_'.join(doc.parameters)}"
+    return f"{doc.return_type} {doc.name} {doc.description} {doc.parameters}"
 
-def parse_chunk(chunk: List[str]) -> Doc:
-    for index, line in enumerate(chunk):
-        if not line.startswith("//"):
-            info = ''.join(chunk[:index]).replace("// ", "")
-            if "struct" in line:
-                name = docs[-1][2:-1]
-                parameters = [x.replace(";", ", ") for x in chunk[index+1:-2]]
-                return Doc(name, "struct", parameters, info)
-            else:
-                signature = ''.join(chunk[index+1:])
-                delim = signature.find('(')
-                name = signature[:delim]
-                parameters = signature[delim+1:-1].split(',')
-                return Doc(name, line, parameters, info)
-    return Doc("", "", list(), "")
+def parse_doc(doc: List[str]) -> Doc:
+    description = None
+    name = None
+    parameters = None
+    return_type = None
+
+    definition = None
+
+    for index, line in enumerate(doc):
+        at_end_of_description = not line.startswith("//")
+        if at_end_of_description:
+            description = doc[:index]
+            definition = doc[index:]
+            break
+
+    if description is None:
+        return Doc(description=description, name=name, parameters=parameters, return_type=return_type)
+
+    if definition is None:
+        return Doc(description=description, name=name, parameters=parameters, return_type=return_type)
+
+    description = [description_line.replace("//", "").strip() for description_line in description]
+
+    return_type_line = definition[0]
+    return_type_is_struct = "struct" in return_type_line
+
+    if return_type_is_struct:
+        return_type = "struct"
+        final_line = definition[-1]
+        name = final_line.split(' ')[-1].replace(';', '')
+        parameters = [parameter.replace(';', '') for parameter in definition[1:-1]]
+    else:
+        return_type = return_type_line
+        signature = ''.join(definition[1:])
+        name_index = signature.find('(')
+        name = signature[:name_index]
+        parameters = signature[name_index:][1:-1].split(", ")
+    return Doc(description=description, name=name, parameters=parameters, return_type=return_type)
+
+
+def get_docs(lines: List[str]) -> List[List[str]]:
+    in_doc = False
+    start_indexes = list()
+    end_indexes = list()
+    for index, line in enumerate(lines):
+        at_start_of_doc = line.startswith("//")
+        at_end_of_doc = line.startswith("{") or len(line.strip()) == 0
+        if in_doc and at_end_of_doc:
+            end_indexes.append(index)
+            in_doc = False
+        elif not in_doc and at_start_of_doc:
+            start_indexes.append(index)
+            in_doc = True
+
+    docs = list()
+
+    indexes = zip(start_indexes, end_indexes)
+    for start_index, end_index in indexes:
+        docs.append([line.strip() for line in lines[start_index:end_index]])
+
+    return docs
 
 
 if __name__ == "__main__":
     lines = get_lines("box.c")
-    in_doc = False
-    start_docs = list()
-    end_docs = list()
-    for i in range(len(lines)):
-        if lines[i].startswith("//"):
-            start_docs.append(i)
-            in_doc = True
-        if (lines[i].startswith("{") or lines[i].startswith("\n")) and in_doc:
-            end_docs.append(i)
-            in_doc = False
-
-    if len(start_docs) != len(end_docs):
-        print("fail")
-
-    for i in range(len(start_docs)):
-        docs = [x.strip() for x in lines[start_docs[i]:end_docs[i]]]
-        print(fmt_doc(parse_chunk(docs)))
+    for doc in get_docs(lines):
+        print(fmt_doc(parse_doc(doc)))
